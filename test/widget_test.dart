15cb89596c44463a107926e38d1510e5f323857e
Ituking <77528963+Ituking@users.noreply.github.com>
2025-07-14 16:11:50 +0900

    test: ウィジェットテストの内容を削除
    - Counterのインクリメントテストを削除し、テスト名を空にしました。

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,19 +1,9 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:presen_neta/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
-
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
