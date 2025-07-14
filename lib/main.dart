661802ebbfd7ece0b544532f25b7853ef06b0e25
Ituking <77528963+Ituking@users.noreply.github.com>
2025-07-14 15:39:25 +0900

    chore: インポートパスの修正
    - app_router.dartのインポートパスを修正し、相対パスから直接パスに変更しました。

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:presen_neta/app/app_router/app_router.dart';
+import 'app/app_router/app_router.dart';
 
 void main() {
   runApp(const MyApp());
