8fc5a36a1bd63ff86692d6f7c0494803bb7fb20f
Ituking <77528963+Ituking@users.noreply.github.com>
2025-07-26 18:29:32 +0900

    fix: インポートの修正
    - `flutter_riverpod`のインポートを`hooks_riverpod`に変更しました。これにより、最新のRiverpodの仕様に対応し、コードの整合性を向上させました。

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:presen_neta/app/app_router/app_router.dart';
 
