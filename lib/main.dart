25a302ac2a4a4fc130adb4fa46f76ed3b9a85f31
Ituking <77528963+Ituking@users.noreply.github.com>
2025-07-26 12:07:01 +0900

    feat: MyAppをProviderScopeでラップして状態管理を追加
    - アプリケーションのエントリーポイントであるmain関数内で、MyAppをProviderScopeでラップしました。
    - これにより、Riverpodを使用した状態管理が可能になりました。

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:presen_neta/app/app_router/app_router.dart';
 
 /// アプリケーションのエントリーポイント。
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 /// アプリ全体のルートウィジェット。
