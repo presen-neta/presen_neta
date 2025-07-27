c3831ca55c6993ffdd0d6b8ea5bbef5376a6d286
Ituking <77528963+Ituking@users.noreply.github.com>
2025-07-27 15:20:56 +0900

    feat: pdfxパッケージを追加し、依存関係を更新
    - `pubspec.yaml`に`pdfx`パッケージの依存関係を追加し、バージョンを`2.9.2`に設定しました。
    - `pubspec.lock`に新たに追加された`pdfx`パッケージの情報を反映しました。
    - iOSおよびWindowsプラットフォームでの`pdfx`プラグインの登録を追加しました。
    - その他の依存関係として、`extension`、`photo_view`、`synchronized`、`universal_platform`などのパッケージ情報も追加しました。

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,12 @@ import Foundation
 
 import file_picker
 import path_provider_foundation
+import pdfx
 import share_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  PdfxPlugin.register(with: registry.registrar(forPlugin: "PdfxPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
 }
